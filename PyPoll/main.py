f2261c3733113e2cadcf288845fdf5b669b3407f
Juan1teno <juan.tenopala@outlook.com>
2023-10-04 14:44:48 -0600

    code for PyPoll, still need to write descriptions format and create the txt file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,5 +2,37 @@ import csv
 
 with open('./Resources/election_data.csv') as data_file:
     csvreader = csv.reader(data_file)
+    
+    csv_header = next(csvreader)
+    id = []
+    candidate = []
+
     for row in csvreader:
-            print(row[0], row[1], row[2])
+        id.append(row[0])
+        candidate.append(row[2])
+
+    votes = len(id)
+    print(votes)
+
+    #set = set(candidate)
+    #print(set)
+    '''
+    {'Raymon Anthony Doane', 'Diana DeGette', 'Charles Casper Stockham'}
+    '''
+    
+    votes_count = {}
+    for item in candidate:
+        votes_count[item] = votes_count.get(item, 0) + 1  
+
+    winner_name = []
+    winner_percentage = []
+    for item, count in votes_count.items():
+        percent = item, (count / votes)*100, count
+        print(percent)
+
+
+        winner_name.append(item)
+        winner_percentage.append(count)
+
+    print(winner_name[winner_percentage.index(max(winner_percentage))])
+
